9dab4124d7c200d099e61504d4cc2624eef26471
Öznur <oznur@tuta.io>
2022-05-11 21:33:36 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Your code goes here
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+  let text = document.getElementById("text");
+  text.textContent = "This is really cool!";
+});
